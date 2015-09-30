1457107163c219ae913fe8adb683da6437c445dd
Jordan Hudgens <jordan@wow.com>
2015-09-30 16:10:09 -0500

    Created seed file with random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+25.times do |ticket|
+  Ticket.create!(title: "Job #{ticket}", description: "This is my job description for job #{ticket}", hours: rand(10), mileage: rand(100), employee: "John Smith", customer: "ABC Company")
+end
