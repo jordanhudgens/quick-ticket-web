4413122579676ca2ad45a32562842cd20726e169
kelseyhilton <kelsey.e.hilton@gmail.com>
2015-11-07 23:08:31 +0000

    Fix duplicate error messages and style flash messages to match bootstrap alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def bootstrap_flash_class(flash_type)
+    { success: 'alert-success',
+      error:   'alert-danger',
+      failure:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-success'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
