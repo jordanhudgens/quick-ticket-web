c24ebcc09f0c669900ef07235cd28a2386e75b40
Jordan Hudgens <jordan@wow.com>
2015-11-09 07:12:41 -0600

    Built out crud functionality for the new Inventory model, created db table and references between the inventory and ticket models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
+  has_many :inventories
 
   validates_presence_of :title, :description, :hours, :customer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013224849) do
+ActiveRecord::Schema.define(version: 20151109131010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inventories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "qty"
+    t.integer  "ticket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "inventories", ["ticket_id"], name: "index_inventories_on_ticket_id", using: :btree
 
   create_table "tickets", force: :cascade do |t|
     t.string   "title"
@@ -48,5 +58,6 @@ ActiveRecord::Schema.define(version: 20151013224849) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "inventories", "tickets"
   add_foreign_key "tickets", "users"
 end
