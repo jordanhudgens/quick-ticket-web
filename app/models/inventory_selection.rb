0f71a822ad2ae6d96f6deb8d419aa40a9e62c6af
Jordan Hudgens <jordan@wow.com>
2015-11-09 11:45:53 -0600

    Integrated a custom validator to ensure there are enough items in the database before saving the item record

--- app/models/inventory_selection.rb
+++ app/models/inventory_selection.rb
@@ -1,4 +1,15 @@
 class InventorySelection < ActiveRecord::Base
   belongs_to :inventory
   belongs_to :ticket
+
+  validate :inventory_qty_available?
+
+  def inventory_qty_available?
+    if self.qty <= Inventory.find(self.inventory_id).qty
+      true
+    else
+      errors.add(:qty, "There aren't enough items in the inventory database.")
+      false
+    end
+  end
 end
