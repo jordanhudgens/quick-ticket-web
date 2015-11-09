6ec7465cc12684fd9b1d4cc164a24323d25bbc30
Jordan Hudgens <jordan@wow.com>
2015-11-09 11:50:09 -0600

    Built custom callback that updates the inventory database after a ticket removes them

--- app/models/inventory_selection.rb
+++ app/models/inventory_selection.rb
@@ -4,6 +4,8 @@ class InventorySelection < ActiveRecord::Base
 
   validate :inventory_qty_available?
 
+  after_create :update_inventory
+
   def inventory_qty_available?
     if self.qty <= Inventory.find(self.inventory_id).qty
       true
@@ -11,5 +13,10 @@ class InventorySelection < ActiveRecord::Base
       errors.add(:qty, "There aren't enough items in the inventory database.")
       false
     end
+  end
+
+  def update_inventory
+    inventory_record = Inventory.find(self.inventory_id)
+    inventory_record.update!(qty: inventory_record.qty - self.qty)
   end
 end
