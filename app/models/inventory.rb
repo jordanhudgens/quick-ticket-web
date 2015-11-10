6de6f14a7e792b2e7140194163d57df1709645c1
Jordan Hudgens <jordan@wow.com>
2015-11-10 09:18:34 -0600

    Integrated validation to ensure negative inventory items cannot be added

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,4 +3,7 @@ class Inventory < ActiveRecord::Base
   has_many :tickets, through: :inventory_selections
 
   validates_presence_of :title, :qty
+
+  validates :qty, :numericality => { :greater_than_or_equal_to => 0 }
+
 end
